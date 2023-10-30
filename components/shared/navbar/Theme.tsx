fb3cb8ce2c3e81caae5e1c0f1695e93fa52d476e
Lomachenko <aggeloskonstantilieris@gmail.com>
2023-10-30 19:58:56 +0200

    finally no errors

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -5,8 +5,6 @@ import {
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import { useTheme } from "@/context/ThemeProvider";
