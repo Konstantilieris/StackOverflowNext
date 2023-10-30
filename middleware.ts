04909000403c252cc08983b7a89f41641803e645
Lomachenko <aggeloskonstantilieris@gmail.com>
2023-10-30 23:58:21 +0200

    new middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "api/webhook",
+    "/api/webhook",
     "question/:id",
     "/tags",
     "/tags/:id",
@@ -18,5 +15,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
